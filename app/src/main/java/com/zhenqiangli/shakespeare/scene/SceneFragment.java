9effa1e0397c8bf67eeab4a81873e091405a5372
Zhenqiang Li <lizhenqiangsnake@gmail.com>
2017-07-30 01:00:27 -0400

    scroll with hiding/showing toolbar

--- app/src/main/java/com/zhenqiangli/shakespeare/scene/SceneFragment.java
+++ app/src/main/java/com/zhenqiangli/shakespeare/scene/SceneFragment.java
@@ -1,5 +1,7 @@
 package com.zhenqiangli.shakespeare.scene;
 
+import android.app.Activity;
+import android.content.ClipData;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,6 +9,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.util.Pair;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +25,7 @@ import com.zhenqiangli.shakespeare.scene.SceneContract.Presenter;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Fragment as View to show scenes of a drama.
@@ -47,24 +52,18 @@ public class SceneFragment extends Fragment implements SceneContract.View {
     scenesView.setLayoutManager(new LinearLayoutManager(getActivity()));
     adapter = new SceneViewAdapter(getActivity());
     scenesView.setAdapter(adapter);
+    scenesView.addOnScrollListener(new HidingScrollListener() {
+      @Override
+      public void onHide() {
+        hideSystemUI(scenesView);
+      }
 
-    hideSystemUI(view);
+      @Override
+      public void onShow() {
+        showSystemUI(scenesView);
+      }
+    });
     return view;
-  }
-
-  // This snippet hides the system bars.
-  // See: https://developer.android.com/training/system-ui/immersive.html#compare
-  private void hideSystemUI(View v) {
-    // Set the IMMERSIVE flag.
-    // Set the content to appear under the system bars so that the content
-    // doesn't resize when the system bars hide and show.
-    v.setSystemUiVisibility(
-            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE);
   }
 
   @Override
@@ -163,5 +162,60 @@ public class SceneFragment extends Fragment implements SceneContract.View {
     }
 
     public abstract void bind(String text);
+  }
+
+  // https://mzgreen.github.io/2015/02/15/How-to-hideshow-Toolbar-when-list-is-scroling%28part1%29/
+  public abstract class HidingScrollListener extends RecyclerView.OnScrollListener {
+    private static final int HIDE_THRESHOLD = 20;
+    private int scrolledDistance = 0;
+    private boolean controlsVisible = true;
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+      super.onScrolled(recyclerView, dx, dy);
+
+      if (scrolledDistance > HIDE_THRESHOLD && controlsVisible) {
+        onHide();
+        controlsVisible = false;
+        scrolledDistance = 0;
+      } else if (scrolledDistance < -HIDE_THRESHOLD && !controlsVisible) {
+        onShow();
+        controlsVisible = true;
+        scrolledDistance = 0;
+      }
+
+      if((controlsVisible && dy>0) || (!controlsVisible && dy<0)) {
+        scrolledDistance += dy;
+      }
+    }
+
+    public abstract void onHide();
+    public abstract void onShow();
+
+  }
+
+  // https://developer.android.com/training/system-ui/immersive.html#compare
+  // This snippet hides the system bars.
+  private void hideSystemUI(View v) {
+    // Set the IMMERSIVE flag.
+    // Set the content to appear under the system bars so that the content
+    // doesn't resize when the system bars hide and show.
+    v.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE);
+  }
+
+  // This snippet shows the system bars. It does this by removing all the flags
+// except for the ones that make the content appear under the system bars.
+  private void showSystemUI(View v) {
+    // TODO: add timer to hide toolbar after 2 seconds
+    v.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
   }
 }

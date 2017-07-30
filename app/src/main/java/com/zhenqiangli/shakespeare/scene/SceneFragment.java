7eeeed3763824943636136571a8c0c4e7dcadd23
Zhenqiang Li <lizhenqiangsnake@gmail.com>
2017-07-30 00:02:10 -0400

    1. add icon 2. refactoring scene recyclerview

--- app/src/main/java/com/zhenqiangli/shakespeare/scene/SceneFragment.java
+++ app/src/main/java/com/zhenqiangli/shakespeare/scene/SceneFragment.java
@@ -1,26 +1,26 @@
 package com.zhenqiangli.shakespeare.scene;
 
 import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.util.Pair;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.Html;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import com.zhenqiangli.shakespeare.R;
-import com.zhenqiangli.shakespeare.data.DataRepository;
 import com.zhenqiangli.shakespeare.data.model.Drama;
 import com.zhenqiangli.shakespeare.data.model.Line;
 import com.zhenqiangli.shakespeare.data.model.Paragraph;
 import com.zhenqiangli.shakespeare.data.model.Scene;
 import com.zhenqiangli.shakespeare.scene.SceneContract.Presenter;
-import com.zhenqiangli.shakespeare.util.HtmlBuilder;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Fragment as View to show scenes of a drama.
@@ -59,8 +59,11 @@ public class SceneFragment extends Fragment implements SceneContract.View {
   public void showDrama(Drama drama, int sceneIndex) {
     adapter.setDrama(drama, sceneIndex);
   }
-  private class SceneViewAdapter extends RecyclerView.Adapter<SceneViewHolder> {
-    private Drama drama;
+  private class SceneViewAdapter extends RecyclerView.Adapter<BaseViewHolder> {
+    private static final int TYPE_SCENE_TITLE = 1;
+    private static final int TYPE_CHARACTER_NAME = 2;
+    private static final int TYPE_SENTENCE = 3;
+    private List<Pair<Integer, String>> items;
     private int sceneIndex;
     private Context context;
 
@@ -68,53 +71,80 @@ public class SceneFragment extends Fragment implements SceneContract.View {
       this.context = context;
     }
 
+    private List<Pair<Integer, String>> from(Drama drama) {
+      List<Pair<Integer, String>> items = new LinkedList<>();
+      for (int i = 0; i < drama.getNumScenes(); i++) {
+        Scene scene = drama.getScene(i);
+        items.add(new Pair<>(TYPE_SCENE_TITLE,
+                String.format("Act %s Scene %s", scene.getActIndex(), scene.getSceneIndex())));
+        for (Paragraph p : scene.getParagraphs().values()) {
+          items.add(new Pair<>(TYPE_CHARACTER_NAME, p.getCharactorName()));
+          for (Line l : p.getLines().values()) {
+            items.add(new Pair<>(TYPE_SENTENCE, l.getContent()));
+          }
+        }
+      }
+      return items;
+    }
+
     void setDrama(Drama drama, int sceneIndex) {
-      this.drama = drama;
+      this.items = from(drama);
       this.sceneIndex = sceneIndex;
       notifyDataSetChanged();
     }
     @Override
-    public SceneViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-      View v = LayoutInflater.from(context).inflate(R.layout.item_scene, parent, false);
-      return new SceneViewHolder(v);
+    public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+      int layoutId = 0;
+      switch (viewType) {
+        case TYPE_CHARACTER_NAME:
+          layoutId = R.layout.item_character_name;
+          break;
+        case TYPE_SCENE_TITLE:
+          layoutId = R.layout.item_scene_title;
+          break;
+        case TYPE_SENTENCE:
+          layoutId = R.layout.item_sentence;
+          break;
+        default:
+          throw new RuntimeException("Unknown type " + viewType);
+      }
+      View v = LayoutInflater.from(context).inflate(layoutId, parent, false);
+      return new TextViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(SceneViewHolder holder, int position) {
-      holder.bind(drama.getScene(position));
+    public void onBindViewHolder(BaseViewHolder holder, int position) {
+      holder.bind(items.get(position).second);
     }
 
     @Override
     public int getItemCount() {
-      return drama.getNumScenes();
+      return items.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+      return items.get(position).first;
     }
   }
 
-  private static class SceneViewHolder extends RecyclerView.ViewHolder {
-    private TextView titleView;
-    private TextView contentView;
-    SceneViewHolder(View v) {
+  private static class TextViewHolder extends BaseViewHolder {
+    TextView contentView;
+    public TextViewHolder(View v) {
       super(v);
-      titleView = (TextView) v.findViewById(R.id.scene_title);
-      contentView = (TextView) v.findViewById(R.id.scene_content);
+      contentView = (TextView) v.findViewById(R.id.item);
+    }
+    @Override
+    public void bind(String text) {
+      contentView.setText(text);
+    }
+  }
+
+  private static abstract class BaseViewHolder extends RecyclerView.ViewHolder {
+    public BaseViewHolder(View v) {
+      super(v);
     }
 
-    void bind(Scene scene) {
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        contentView.setText(Html.fromHtml(parseScene(scene), Html.TO_HTML_PARAGRAPH_LINES_INDIVIDUAL));
-      }
-      titleView.setText(String.format("Act %s Scene %s", scene.getActIndex(), scene.getSceneIndex()));
-    }
-
-    private String parseScene(Scene scene) {
-      HtmlBuilder htmlBuilder = new HtmlBuilder();
-      for (Paragraph p : scene.getParagraphs().values()) {
-        htmlBuilder.h1(p.getCharactorName()).br();
-        for (Line l : p.getLines().values()) {
-          htmlBuilder.append(l.getContent()).br();
-        }
-      }
-      return htmlBuilder.build();
-    }
+    public abstract void bind(String text);
   }
 }
